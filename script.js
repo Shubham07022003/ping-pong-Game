a20d67ce00cca1820d3f86663ae4a0072833ecef
syntaxSmith_07 <kumarshubham07022003@gmail.com>
2025-05-07 14:01:08 +0530

    add paddle functionlity

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     let table = document.getElementById("ping-pong-table");
     let ball = document.getElementById("ball");
+    let paddle = document.getElementById("paddle");
 
     let ballX =50; //distance of the top of ball wrt ping pong able
     let ballY = 50; //distance of the left of ball wrt ping pong able
@@ -20,5 +21,21 @@ document.addEventListener("DOMContentLoaded", function () {
         if(ballX> table.offsetWidth - ball.offsetWidth || ballX <= 0) dx*=-1; //if ball hits the right or left wall, reverse the direction of ball in x direction
         if(ballY> table.offsetHeight - ball.offsetHeight || ballY <= 0) dy*=-1; //if ball hits the bottom or top wall, reverse the direction of ball in y direction
 
+    }, 1);
+
+    let paddleY = 0;
+    let dPy =5; //distance factor in y direction
+    document.addEventListener("keydown", (event)=> {
+        event.preventDefault(); //to prevent the default action of the key pressed
+        if(event.keyCode == 38 && paddleY > 0){
+            // ip arrow
+            paddleY +=(-1)*dPy;
+            console.log("up", paddleY);
+        }else if(event.keyCode ==40 && paddleY < table.offsetHeight - paddle.offsetHeight){
+            // down arrow
+            paddleY += dPy;
+            console.log("down", paddleY);
+        }
+        paddle.style.top= `${paddleY}px`;
     })
 })
