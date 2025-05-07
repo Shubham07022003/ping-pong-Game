9e8c91a179f2a651b2fdd4185edaf08d6f1802c6
syntaxSmith_07 <kumarshubham07022003@gmail.com>
2025-05-07 14:17:37 +0530

    add paddle and ball collision logic

--- script.js
+++ script.js
@@ -18,13 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
         ball.style.top = `${ballY}px`;
     //     if(ballX> 700-20 || ballX <= 0) dx*=-1; //if ball hits the right or left wall, reverse the direction of ball in x direction
     //     if(ballY> 400-20 || ballY <= 0) dy*=-1; //if ball hits the bottom or top wall, reverse the direction of ball in y direction
+        if(ballX< paddle.offsetLeft + paddle.offsetWidth && ballY > paddle.offsetLeft && ballY  - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight){
+            // if ball hits the paddle, reverse the direction of ball in x direction
+            dx*=-1;
+           // console.log("hit",ballX,ballY);
+        }
         if(ballX> table.offsetWidth - ball.offsetWidth || ballX <= 0) dx*=-1; //if ball hits the right or left wall, reverse the direction of ball in x direction
         if(ballY> table.offsetHeight - ball.offsetHeight || ballY <= 0) dy*=-1; //if ball hits the bottom or top wall, reverse the direction of ball in y direction
 
     }, 1);
 
     let paddleY = 0;
-    let dPy =5; //distance factor in y direction
+    let dPy =10; //distance factor in y direction
     document.addEventListener("keydown", (event)=> {
         event.preventDefault(); //to prevent the default action of the key pressed
         if(event.keyCode == 38 && paddleY > 0){
